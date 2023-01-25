6c518e53a863b0faf9d985b42d83ff4b3a4103c9
Nick Mittu <nick@mittudev.com>
2023-01-25 02:41:57 -0500

    Update secret name

--- bin/main.ml
+++ bin/main.ml
@@ -4,7 +4,7 @@ let () =
   let gradescope_token = Gradescope.get_signed_token () in
   let github_auth = Github.get_auth () in
   let github_repo = Github.get_current_repo github_auth in
-  if Github.set_secret github_repo ~key:"GRADESCOPE_TOK" ~value:gradescope_token then
+  if Github.set_secret github_repo ~key:"GRADESCOPE_TOKEN" ~value:gradescope_token then
     printf "Successfully set up github repo\n"
   else
     printf "There was an issue setting up your github repo. Please see a TA\n"
